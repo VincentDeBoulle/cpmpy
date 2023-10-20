68cb38bb1fb2bff6588fbae768c4360b7b5662d7
VincentDeBoulle <vincent.de.boulle@gmail.com>
2023-10-20 13:32:39 +0200

    Create a benchmark file

---
+++ examples/csplib/benchmark.py
@@ -0,0 +1,22 @@
+import subprocess
+
+files = ["examples/csplib/prob001_car_sequence.py", 
+         "examples/csplib/prob005_auto_correlation.py", 
+         "examples/csplib/prob006_golomb.py",
+         "examples/csplib/prob007_all_interval.py",
+         "examples/csplib/prob009_perfect_squares.py",
+         "examples/csplib/prob026_sport_scheduling.py",
+         "examples/csplib/prob044_steiner.py",
+         "examples/csplib/prob054_n_queens.py",
+         "examples/csplib/prob076_costas_arrays.py"
+         ]
+
+for file_name in files:
+    try:
+        print(file_name)
+        cmd = ["python", file_name]
+        subprocess.run(cmd)
+    except FileNotFoundError:
+        print("Error: file {} not found".format(file_name))
+    except Exception as e:
+        print("Error running {}: {}".format(file_name, e))
