b560b47dd4cd60a34ac218508d9a50a5320eaf0c
VincentDeBoulle <vincent.de.boulle@gmail.com>
2023-12-09 14:36:06 +0100

    Added CSE

---
+++ cpmpy/transformations/comparison_CSE.py
@@ -0,0 +1,47 @@
+import copy
+
+from .flatten_model_CSE import get_or_make_var
+from ..expressions.core import Comparison
+from ..expressions.variables import _NumVarImpl
+
+"""
+  Transformations regarding Comparison constraints.
+  
+  Comparisons in Flat Normal Form are of the kind
+    - NumExpr == IV
+    - BoolExpr == BV
+    
+  The latter is a reified expression, not considered here.
+  (for handling of all reified expressions, see `reification.py` transformations)
+  
+  This file implements:
+    - only_numexpr_equality():    transforms `NumExpr <op> IV` to `(NumExpr == A) & (A <op> IV)` if not supported
+"""
+
+def only_numexpr_equality(constraints, supported=frozenset(), expr_dict=None):
+    """
+        transforms `NumExpr <op> IV` to `(NumExpr == A) & (A <op> IV)` if not supported
+
+        :param supported  a (frozen)set of expression names that supports all comparisons in the solver
+    """
+    if expr_dict is None:
+        expr_dict = dict()
+
+    # shallow copy (could support inplace too this way...)
+    newcons = copy.copy(constraints)
+
+    for i,con in enumerate(newcons):
+        if isinstance(con, Comparison) and con.name != '==':
+            # LHS <op> IV    with <op> one of !=,<,<=,>,>=
+            lhs = con.args[0]
+            if not isinstance(lhs, _NumVarImpl) and not lhs.name in supported:
+                # LHS is unsupported for LHS <op> IV, rewrite to `(LHS == A) & (A <op> IV)`
+                (lhsvar, lhscons) = get_or_make_var(lhs, expr_dict=expr_dict)
+                # replace comparison by A <op> IV
+                newcons[i] = Comparison(con.name, lhsvar, con.args[1])
+                # add lhscon(s), which will be [(LHS == A)]
+                if len(lhscons) == 1:
+                  newcons.insert(i, lhscons[0])
+                else:
+                    assert(len(lhscons) == 0), "only_numexpr_eq: lhs surprisingly non-flat" # can be 0 because of CSE
+    return newcons
