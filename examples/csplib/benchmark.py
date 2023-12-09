e86c78986bc213c856b7ee8228751bac6125cf39
VincentDeBoulle <vincent.de.boulle@gmail.com>
2023-12-09 13:42:57 +0100

    Created a benchmark

---
+++ examples/csplib/benchmark.py
@@ -0,0 +1,32 @@
+import subprocess
+
+files = [
+    "examples/csplib/prob001_car_sequence.py",
+    "examples/csplib/prob002_template_design.py",
+    "examples/csplib/prob005_auto_correlation.py",
+    "examples/csplib/prob006_golomb.py",
+    "examples/csplib/prob007_all_interval.py",
+    "examples/csplib/prob008_vessel_loading.py",
+    "examples/csplib/prob009_perfect_squares.py",
+    "examples/csplib/prob019_magic_sequence.py",
+    "examples/csplib/prob026_sport_scheduling.py",
+    "examples/csplib/prob028_bibd.py",
+    "examples/csplib/prob033_word_design.py",
+    "examples/csplib/prob044_steiner.py",
+    "examples/csplib/prob049_number_partitioning.py",
+    "examples/csplib/prob050_diamond_free.py",
+    "examples/csplib/prob054_n_queens.py",
+    "examples/csplib/prob076_costas_arrays.py",
+    "examples/csplib/prob084_hadamard_matrix.py",
+    "examples/csplib/prob110_peaceably_co-existing_armies_of_queens.py"
+]
+
+for file_name in files:
+    try:
+        print(file_name)
+        cmd = ["python", file_name]
+        subprocess.run(cmd)
+    except FileNotFoundError:
+        print("Error: file {} not found".format(file_name))
+    except Exception as e:
+        print("Error running {}: {}".format(file_name, e))
---
+++ examples/csplib/prob110_peaceably_co-existing_armies_of_queens.py
@@ -0,0 +1,114 @@
+"""
+Peaceably co-existing armies of queens in CPMpy
+
+CSPlib prob110
+https://www.csplib.org/Problems/prob110/models/PeacableArmies.py.html
+
+Problem description from CSPlib website:
+In the "Armies of queens" problem, we are required to place two equal-sized armies
+of black and white queens on a chessboard so that the white queens do not attack
+the black queens (and necessarily visa versa) and to find the maximum size of the
+two armies.
+
+This CPMpy model was written by Vincent De Boulle
+"""
+# Add the correct path
+import sys
+sys.path.append('../cpmpy')
+
+# Load the libraries
+from cpmpy import *
+import timeit
+from prettytable import PrettyTable
+import gc
+
+def peaceable_queens(n=8):
+
+    b = intvar(0, 1, shape=(n,n))
+    w = intvar(0, 1, shape=(n,n))
+
+    model = Model()
+
+    num_black_queens = sum(b)
+    num_white_queens = sum(w)
+
+    model += (num_black_queens == num_white_queens)
+    model.objective(num_black_queens, minimize=False)
+
+    for bx in range(n):
+        for by in range(n):
+            for wx in range(n):
+                for wy in range(n):
+                    if (bx, by)  == (wx, wy):
+                        model += b[bx][by] + w[wx][wy] <= 1
+                    if bx < wx or (bx == wx and by < wy):
+                        if bx == wx or by == wy or abs(bx - wx) == abs(by - wy):
+                            model += b[bx][by] + w[wx][wy] <= 1
+                            model += w[bx][by] + b[wx][wy] <= 1
+    return model, b,w
+
+if __name__ == "__main__":
+
+    nb_iterations = 10
+
+    tablesp_ortools = PrettyTable(['Board size', 'Model Creation Time', 'Solver Creation + Transform Time', 'Solve Time', 'Overall Execution Time', 'Number of Search Branches'])
+    tablesp_ortools.title = f'OR-Tools: Results of the Peaceably Co-existing Armies of Queens problem with CSE (average of {nb_iterations} iterations)'
+    tablesp_ortools_noCSE =  PrettyTable(['Board size', 'Model Creation Time', 'Solver Creation + Transform Time', 'Solve Time', 'Overall Execution Time', 'Number of Search Branches'])
+    tablesp_ortools_noCSE.title = f'OR-Tools: Results of the Peaceably Co-existing Armies of Queens problem without CSE (average of {nb_iterations} iterations)'
+    
+    for sz in range(5, 21):
+        n = sz # Size of the board
+        print('Size: ', n)
+        
+        def run_code(slvr):
+            start_model_time = timeit.default_timer()
+            model, black_queens, white_queens = peaceable_queens(n)
+            model_creation_time = timeit.default_timer() - start_model_time
+            return model.solve(solver=slvr), model_creation_time, black_queens, white_queens
+        
+        for slvr in ['ortools']:
+            total_model_creation_time = []
+            total_transform_time = []
+            total_solve_time = []
+            total_execution_time = []
+            total_num_branches = []
+
+            for lp in range(nb_iterations):
+                gc.disable()
+
+                start_time = timeit.default_timer()
+                (n_sols, transform_time, solve_time, num_branches), model_creation_time, black_queens, white_queens = run_code(slvr)
+                execution_time = timeit.default_timer() - start_time
+
+                line = '+---'*n +'+\n'
+                out = line
+                for x in range(n):
+                    for y in range(n):
+                        if black_queens.value()[x][y] == 1:
+                            out += '| B '
+                        elif white_queens.value()[x][y] == 1:
+                            out += '| W '
+                        else:
+                            out += '|   '
+                    out += '|\n' + line
+                print(out)
+                
+                total_model_creation_time.append(model_creation_time)
+                total_transform_time.append(transform_time)
+                total_solve_time.append(solve_time)
+                total_execution_time.append(execution_time)
+                total_num_branches.append(num_branches)
+
+                gc.enable()
+
+            average_model_creation_time = sum(sorted(total_model_creation_time)[:3]) / 3 
+            average_transform_time = sum(sorted(total_transform_time)[:3]) / 3 
+            average_solve_time = sum(sorted(total_solve_time)[:3]) / 3 
+            average_execution_time = sum(sorted(total_execution_time)[:3]) / 3 
+            average_num_branches = sum(sorted(total_num_branches)[:3]) / 3
+
+            if slvr == 'ortools':
+                tablesp_ortools.add_row([n, average_model_creation_time, average_transform_time, average_solve_time, average_execution_time, average_num_branches])
+                with open("cpmpy/timing_results/peaceably_queens.txt", "w") as f:
+                    f.write(str(tablesp_ortools))
+                    f.write("\n")
