33d9dcae7d73e47ead32ed40d5e7f99249078585
VincentDeBoulle <vincent.de.boulle@gmail.com>
2024-02-13 09:50:33 +0100

    Feedback

--- examples/csplib/prob008_vessel_loading.py
+++ examples/csplib/prob008_vessel_loading.py
@@ -159,11 +159,11 @@ if __name__ == "__main__":
                 gc.enable()
 
             if slvr == 'ortools':
-                average_model_creation_time = sum(sorted(total_model_creation_time)[:3]) / 3 
-                average_transform_time = sum(sorted(total_transform_time)[:3]) / 3 
-                average_solve_time = sum(sorted(total_solve_time)[:3]) / 3 
-                average_execution_time = sum(sorted(total_execution_time)[:3]) / 3 
-                average_num_branches = sum(sorted(total_num_branches)[:3]) / 3
+                average_model_creation_time = sum(total_model_creation_time) / nb_iterations
+                average_transform_time = sum(total_transform_time) / nb_iterations
+                average_solve_time = sum(total_solve_time) / nb_iterations
+                average_execution_time = sum(total_execution_time) / nb_iterations
+                average_num_branches = sum(total_num_branches) / nb_iterations
 
                 tablesp_ortools.add_row([name, average_model_creation_time, average_transform_time, average_solve_time, average_execution_time, average_num_branches])
                 with open("cpmpy/timing_results/vessel_loading.txt", "w") as f:
@@ -171,11 +171,11 @@ if __name__ == "__main__":
                     f.write("\n")
 
             elif slvr == 'ortools_CSE':
-                average_model_creation_time_2 = sum(sorted(total_model_creation_time)[:3]) / 3 
-                average_transform_time_2 = sum(sorted(total_transform_time)[:3]) / 3 
-                average_solve_time_2 = sum(sorted(total_solve_time)[:3]) / 3 
-                average_execution_time_2 = sum(sorted(total_execution_time)[:3]) / 3 
-                average_num_branches_2 = sum(sorted(total_num_branches)[:3]) / 3
+                average_model_creation_time_2 = sum(total_model_creation_time) / nb_iterations 
+                average_transform_time_2 = sum(total_transform_time) / nb_iterations
+                average_solve_time_2 = sum(total_solve_time) / nb_iterations
+                average_execution_time_2 = sum(total_execution_time) / nb_iterations
+                average_num_branches_2 = sum(total_num_branches) / nb_iterations
 
                 tablesp_ortools_CSE.add_row([name, average_model_creation_time_2, average_transform_time_2, average_solve_time_2, average_execution_time_2, average_num_branches_2])
                 with open("cpmpy/timing_results/vessel_loading_CSE.txt", "w") as f:
