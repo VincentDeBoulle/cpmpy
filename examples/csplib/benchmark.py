0809eb07957b58da70a7290934bd3a81224d965d
VincentDeBoulle <vincent.de.boulle@gmail.com>
2024-02-20 08:32:11 +0100

    Baseline

---
+++ examples/csplib/benchmark.py
@@ -0,0 +1,18 @@
+import subprocess
+
+files = [
+    "examples/csplib/prob005_auto_correlation.py",
+    "examples/csplib/prob007_all_interval.py",
+    "examples/csplib/prob049_number_partitioning.py",
+    "examples/csplib/prob054_n_queens.py",
+]
+
+for file_name in files:
+    try:
+        print(file_name)
+        cmd = ["python", file_name]
+        subprocess.run(cmd)
+    except FileNotFoundError:
+        print("Error: file {} not found".format(file_name))
+    except Exception as e:
+        print("Error running {}: {}".format(file_name, e))
