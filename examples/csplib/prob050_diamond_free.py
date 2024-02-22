bc93b6554156654a828e6704b41355e9bf332415
VincentDeBoulle <vincent.de.boulle@gmail.com>
2024-02-22 12:40:52 +0100

    New examples

--- examples/csplib/prob050_diamond_free.py
+++ examples/csplib/prob050_diamond_free.py
@@ -26,11 +26,18 @@ See also my cpmpy page: http://www.hakank.org/cpmpy/
 Modified by Ignace Bleukx, ignace.bleukx@kuleuven.be
 
 """
+import random
 import sys
 import numpy as np
+import gc
+
+sys.path.append('../cpmpy')
+
 from cpmpy import *
 from cpmpy.solvers import *
 from itertools import combinations
+import timeit
+from prettytable import PrettyTable
 
 def diamond_free(N=10):
     # By definition a and b will have the same cardinality:
@@ -82,15 +89,88 @@ def print_sol(matrix):
 if __name__ == "__main__":
     import argparse
 
-    parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter)
-    parser.add_argument("-n", type=int, default=10, help="Size of diamond")
-    parser.add_argument("--solution-limit", type=int, default=0, help="Number of solutions to find, find all by default")
+    nb_iterations = 10
 
-    args = parser.parse_args()
+    tablesp_ortools =  PrettyTable(['Size of diamond', 'Number of Solutions', 'Model Creation Time', 'Solver Creation + Transform Time', 'Solve Time', 'Overall Execution Time', 'number of search branches'])
+    tablesp_ortools.title = 'Results of the Diamond Free problem without CSE'
+    tablesp_ortools_CSE =  PrettyTable(['Size of diamond', 'Number of Solutions', 'Model Creation Time', 'Solver Creation + Transform Time', 'Solve Time', 'Overall Execution Time', 'number of search branches'])
+    tablesp_ortools_CSE.title = 'Results of the Diamond Free problem with CSE'    
+    tablesp_ortools_factor =  PrettyTable(['Size of diamond', 'Number of Solutions', 'Model Creation Time', 'Solver Creation + Transform Time', 'Solve Time', 'Overall Execution Time', 'number of search branches'])
+    tablesp_ortools_factor.title = 'Results of the Diamond Free problem'    
 
-    model, matrix = diamond_free(args.n)
-    num_sol = model.solveAll(
-        solution_limit=args.solution_limit,
-        display=lambda : print_sol(matrix))
-    print("num_solutions:",num_sol)
+    for nb in range(10, 21):
+        parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter)
+        parser.add_argument("-n", type=int, default=nb, help="Size of diamond")
+        parser.add_argument("--solution-limit", type=int, default=0, help="Number of solutions to find, find all by default")
 
+        args = parser.parse_args()
+        print(args.n)
+
+        def run_code(slvr):
+            start_model_time = timeit.default_timer()
+            model, matrix = diamond_free(args.n)
+            model_creation_time = timeit.default_timer() - start_model_time
+            return model.solve(
+                solver=slvr,
+                time_limit=30), model_creation_time
+
+        for slvr in ["ortools", "ortools_CSE"]:
+            total_model_creation_time = []
+            total_transform_time = []
+            total_solve_time = []
+            total_execution_time = []
+            total_num_branches = []
+
+            for lp in range(nb_iterations):
+                random.seed(lp)
+
+                # Disable garbage collection for timing measurements
+                gc.disable()
+
+                # Measure the model creation and execution time
+                start_time = timeit.default_timer()
+                (n_sols, transform_time, solve_time, num_branches), model_creation_time = run_code(slvr)
+                execution_time = timeit.default_timer() - start_time
+
+                total_model_creation_time.append(model_creation_time)
+                total_transform_time.append(transform_time)
+                total_solve_time.append(solve_time)
+                total_execution_time.append(execution_time)
+                total_num_branches.append(num_branches)
+
+                # Re-enable garbage collection
+                gc.enable()
+
+            if slvr == 'ortools':
+                average_model_creation_time = sum(total_model_creation_time) / nb_iterations 
+                average_transform_time = sum(total_transform_time) / nb_iterations
+                average_solve_time = sum(total_solve_time) / nb_iterations 
+                average_execution_time = sum(total_execution_time) / nb_iterations 
+                average_num_branches = sum(total_num_branches) / nb_iterations 
+
+                tablesp_ortools.add_row([nb, n_sols, average_model_creation_time, average_transform_time, average_solve_time, average_execution_time, average_num_branches])
+                with open("cpmpy/timing_results/diamond_free.txt", "w") as f:
+                    f.write(str(tablesp_ortools))
+                    f.write("\n")
+            elif slvr == 'ortools_CSE':
+                average_model_creation_time_2 = sum(total_model_creation_time) / nb_iterations
+                average_transform_time_2 = sum(total_transform_time) / nb_iterations
+                average_solve_time_2 = sum(total_solve_time) / nb_iterations
+                average_execution_time_2 = sum(total_execution_time) / nb_iterations 
+                average_num_branches_2 = sum(total_num_branches) / nb_iterations
+
+                tablesp_ortools_CSE.add_row([nb, n_sols, average_model_creation_time_2, average_transform_time_2, average_solve_time_2, average_execution_time_2, average_num_branches_2])
+                with open("cpmpy/timing_results/diamond_free_CSE.txt", "w") as f:
+                    f.write(str(tablesp_ortools_CSE))
+                    f.write("\n")
+
+                factor_model_creation_time = average_model_creation_time / average_model_creation_time_2
+                factor_tranform_time = average_transform_time / average_transform_time_2
+                factor_solve_time = average_solve_time / average_solve_time_2
+                factor_execution_time = average_execution_time / average_execution_time_2
+                factor_num_branches = average_num_branches / average_num_branches_2
+
+                tablesp_ortools_factor.add_row([nb, n_sols, factor_model_creation_time, factor_tranform_time, factor_solve_time, factor_execution_time, factor_num_branches])
+                with open("cpmpy/CSE_results/diamond_free.txt", "w") as f:
+                    f.write(str(tablesp_ortools_factor))
+                    f.write("\n")
