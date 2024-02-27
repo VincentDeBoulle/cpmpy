3ea65a7e8d9543517fb3bcd6f6e6c69094e2eaba
VincentDeBoulle <vincent.de.boulle@gmail.com>
2024-02-27 14:28:50 +0100

    undo merge

--- cpmpy/solvers/ortools_CSE.py
+++ cpmpy/solvers/ortools_CSE.py
@@ -332,7 +332,6 @@ class CPM_ortools_CSE(SolverInterface):
         cpm_cons = toplevel_list(cpm_expr)
         supported = {"min", "max", "abs", "element", "alldifferent", "xor", "table", "cumulative", "circuit", "inverse"}
         cpm_cons = decompose_in_tree(cpm_cons, supported)
-        cpm_cons = remove_redundant(cpm_cons)
         cpm_cons = flatten_constraint(cpm_cons, expr_dict=self.expr_dict)  # flat normal form
         cpm_cons = reify_rewrite(cpm_cons, supported=frozenset(['sum', 'wsum']), expr_dict=self.expr_dict)  # constraints that support reification
         cpm_cons = only_numexpr_equality(cpm_cons, supported=frozenset(["sum", "wsum", "sub"]), expr_dict=self.expr_dict)  # supports >, <, !=
