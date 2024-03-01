7b2765a785d1592ff379a97d9fd73708d884b415
VincentDeBoulle <vincent.de.boulle@gmail.com>
2024-03-01 15:35:05 +0100

    Benchmark updated

--- examples/CSE-problems/duplicates.py
+++ examples/CSE-problems/duplicates.py
@@ -1,66 +1,51 @@
-import sys
 import gc
 import random
+import sys
 import timeit
 
+from prettytable import PrettyTable
 sys.path.append('../cpmpy')
 
 from cpmpy import *
-from prettytable import PrettyTable
-from itertools import permutations
 
-A = intvar(0, 1000)
-B = intvar(0, 1000)
-C = intvar(0, 1000)
-D = intvar(0, 1000)
-E = intvar(0, 1000)
-F = intvar(0, 1000)
-G = intvar(0, 1000)
-H = intvar(0, 1000)
-I = intvar(0, 1000)
-J = intvar(0, 1000)
-K = intvar(0, 1000)
-L = intvar(0, 1000)
-M = intvar(0, 1000)
-N = intvar(0, 1000)
-O = intvar(0, 1000)
-P = intvar(0, 1000)
+A = intvar(0, 100)
+B = intvar(0, 100)
+C = intvar(0, 100)
+D = intvar(0, 100)
+E = intvar(0, 100)
+F = intvar(0, 100)
 
-def atest(n = 100):
+
+def duplicates(n= 10000):
     model = Model()
 
-    model += A + B == 50
-    model += B + A == C + D
-    model += ((A + B) + C) + D == E
-
+    model += A + B == 10
+    model += A + B == C
     for i in range(n):
-        lst = [F, G, H, I, J]
-        p = permutations(lst)
-
-        for per in p:
-            model += per[0] + per[1] + per[2] + per[3] + per [4] == O + P
-    
+        model += A + B  == E + F
+        model += C + D == A + B
     return model
+
 
 if __name__ == "__main__":
     nb_iterations = 10
 
-    tablesp_ortools =  PrettyTable(['Number of repetitions', 'Model Creation Time', 'Solver Creation + Transform Time', 'Solve Time', 'Overall Execution Time', 'number of search branches'])
-    tablesp_ortools.title = 'Results of a problem with a lot of duplicates without CSE'
-    tablesp_ortools_CSE =  PrettyTable(['Number of repetitions', 'Model Creation Time', 'Solver Creation + Transform Time', 'Solve Time', 'Overall Execution Time', 'number of search branches'])
-    tablesp_ortools_CSE.title = 'Results of a problem with a lot of duplicates with CSE'    
-    tablesp_ortools_factor =  PrettyTable(['Number of repetitions', 'Model Creation Time', 'Solver Creation + Transform Time', 'Solve Time', 'Overall Execution Time', 'number of search branches'])
-    tablesp_ortools_factor.title = 'Results of a problem with a lot of duplicates'
+    tablesp_ortools =  PrettyTable(['Numbers of repetitions', 'Model Creation Time', 'Solver Creation + Transform Time', 'Solve Time', 'Overall Execution Time', 'number of search branches'])
+    tablesp_ortools.title = 'Results of the Duplicates problem without CSE'
+    tablesp_ortools_CSE =  PrettyTable(['Numbers of repetitions', 'Model Creation Time', 'Solver Creation + Transform Time', 'Solve Time', 'Overall Execution Time', 'number of search branches'])
+    tablesp_ortools_CSE.title = 'Results of the Duplicates problem with CSE'    
+    tablesp_ortools_factor =  PrettyTable(['Numbers of repetitions', 'Model Creation Time', 'Solver Creation + Transform Time', 'Solve Time', 'Overall Execution Time', 'number of search branches'])
+    tablesp_ortools_factor.title = 'Results of the Duplicates problem'
 
-    for n in range(10, 200, 10):
+    for n in range(1000, 100000, 500):
 
         def create_model():
-            return atest(n)
+            return duplicates(n)
         model_creation_time = timeit.timeit(create_model, number = 1)    
 
         def run_code(slvr):
             start_model_time = timeit.default_timer()
-            model = atest(n)
+            model = duplicates(n)
             model_creation_time = timeit.default_timer() - start_model_time
             return model.solve(solver=slvr, time_limit=30), model_creation_time
 
@@ -99,7 +84,7 @@ if __name__ == "__main__":
                 average_num_branches = sum(total_num_branches) / nb_iterations 
 
                 tablesp_ortools.add_row([n, average_model_creation_time, average_transform_time, average_solve_time, average_execution_time, average_num_branches])
-                with open("cpmpy/timing_results/atest.txt", "w") as f:
+                with open("cpmpy/timing_results/duplicates.txt", "w") as f:
                     f.write(str(tablesp_ortools))
                     f.write("\n")
 
@@ -111,7 +96,7 @@ if __name__ == "__main__":
                 average_num_branches_2 = sum(total_num_branches) / nb_iterations
 
                 tablesp_ortools_CSE.add_row([n, average_model_creation_time_2, average_transform_time_2, average_solve_time_2, average_execution_time_2, average_num_branches_2])
-                with open("cpmpy/timing_results/atest.txt", "w") as f:
+                with open("cpmpy/timing_results/duplicates.txt", "w") as f:
                     f.write(str(tablesp_ortools_CSE))
                     f.write("\n")
 
@@ -122,6 +107,6 @@ if __name__ == "__main__":
                 factor_num_branches = average_num_branches / average_num_branches_2
 
                 tablesp_ortools_factor.add_row([n, factor_model_creation_time, factor_tranform_time, factor_solve_time, factor_execution_time, factor_num_branches])
-                with open("cpmpy/CSE_results/atest_CSE.txt", "w") as f:
+                with open("cpmpy/CSE_results/duplicates.txt", "w") as f:
                     f.write(str(tablesp_ortools_factor))
                     f.write("\n")
