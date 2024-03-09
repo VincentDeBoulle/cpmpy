e668cfa490f065a310389ecb3d95e9cb09b249dd
VincentDeBoulle <vincent.de.boulle@gmail.com>
2024-03-09 10:55:52 +0100

    Test

--- examples/CSE-problems/test.py
+++ examples/CSE-problems/test.py
@@ -11,8 +11,9 @@ B = intvar(0, 10)
 C = intvar(0, 10)
 D = intvar(0, 10)
 E = intvar(0, 10)
+F = intvar(0, 10)
 
-model += A + B == C
+model += A + B == C + F
 model += A + B == D + E
 
 model.solve(solver="ortools_CSE")
