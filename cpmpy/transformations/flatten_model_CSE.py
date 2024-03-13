82ca6c2c3ce3d1d23f77016f59906cf10dde5036
VincentDeBoulle <vincent.de.boulle@gmail.com>
2024-03-13 09:33:00 +0100

    Added horn clauses

--- cpmpy/transformations/flatten_model_CSE.py
+++ cpmpy/transformations/flatten_model_CSE.py
@@ -595,3 +595,26 @@ def applydemorgan(cpm_expr):
         else:
             new_expr_list.append(expr)
     return new_expr_list
+
+def horn_clauses(cpm_cons):
+    new_expr_list = []
+
+    for expr in cpm_cons:
+        if isinstance(expr, Operator) and expr.name == 'or':
+            pos_vars = sorted([arg for arg in expr.args if not isinstance(arg, NegBoolView)], key=str)
+
+            if len(pos_vars) == 1:
+                neg_vars = [~arg for arg in expr.args if isinstance(arg, NegBoolView)]
+                lhs = Operator('and', neg_vars)
+                new_expr = Operator('->', [lhs, pos_vars[0]])
+                new_expr_list.append(new_expr)
+            else:
+                rhs = pos_vars[0]
+                pos_vars.pop(0)
+                neg_vars = [~arg for arg in expr.args if isinstance(arg, NegBoolView)] + [~arg for arg in pos_vars]
+                lhs = Operator('and', neg_vars)
+                new_expr = Operator('->', [lhs, rhs])
+                new_expr_list.append(new_expr)
+        else:
+            new_expr_list.append(expr)
+        return new_expr_list
