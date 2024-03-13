b7a0b097366bae9f8e691ff91857e6a860896f38
VincentDeBoulle <vincent.de.boulle@gmail.com>
2024-03-13 08:50:04 +0100

    Remove redundant

--- cpmpy/solvers/ortools_CSE.py
+++ cpmpy/solvers/ortools_CSE.py
@@ -26,6 +26,8 @@
 import sys  # for stdout checking
 import numpy as np
 
+from cpmpy.transformations.CSE_transformations import remove_redundant
+
 from .solver_interface import SolverInterface, SolverStatus, ExitStatus
 from ..exceptions import NotSupportedError
 from ..expressions.core import Expression, Comparison, Operator, BoolVal
@@ -330,6 +332,7 @@ class CPM_ortools_CSE(SolverInterface):
         cpm_cons = toplevel_list(cpm_expr)
         supported = {"min", "max", "abs", "element", "alldifferent", "xor", "table", "cumulative", "circuit", "inverse"}
         cpm_cons = decompose_in_tree(cpm_cons, supported)
+        cpm_cons = remove_redundant(cpm_cons)
         cpm_cons = flatten_constraint(cpm_cons, expr_dict=self.expr_dict)  # flat normal form
         cpm_cons = reify_rewrite(cpm_cons, supported=frozenset(['sum', 'wsum']), expr_dict=self.expr_dict)  # constraints that support reification
         cpm_cons = only_numexpr_equality(cpm_cons, supported=frozenset(["sum", "wsum", "sub"]), expr_dict=self.expr_dict)  # supports >, <, !=
