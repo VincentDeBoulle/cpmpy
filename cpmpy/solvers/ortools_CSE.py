c68d28170ccf2adf70ee62d3ecdaae8c62ce9df3
VincentDeBoulle <vincent.de.boulle@gmail.com>
2024-04-25 14:05:12 +0200

    Created new experiment

--- cpmpy/solvers/ortools_CSE.py
+++ cpmpy/solvers/ortools_CSE.py
@@ -26,7 +26,7 @@
 import sys  # for stdout checking
 import numpy as np
 
-from cpmpy.transformations.CSE_transformations import remove_redundant
+from cpmpy.transformations.CSE_transformations import horn_clauses, remove_redundant
 
 from .solver_interface import SolverInterface, SolverStatus, ExitStatus
 from ..exceptions import NotSupportedError
@@ -332,6 +332,7 @@ class CPM_ortools_CSE(SolverInterface):
         cpm_cons = toplevel_list(cpm_expr)
         supported = {"min", "max", "abs", "element", "alldifferent", "xor", "table", "cumulative", "circuit", "inverse"}
         cpm_cons = decompose_in_tree(cpm_cons, supported)
+        cpm_cons = horn_clauses(cpm_cons)
         cpm_cons = remove_redundant(cpm_cons)
         cpm_cons = flatten_constraint(cpm_cons, expr_dict=self.expr_dict)  # flat normal form
         cpm_cons = reify_rewrite(cpm_cons, supported=frozenset(['sum', 'wsum']), expr_dict=self.expr_dict)  # constraints that support reification
--- cpmpy/transformations/CSE_transformations.py
+++ cpmpy/transformations/CSE_transformations.py
@@ -1,4 +1,5 @@
-from ..expressions.core import Comparison
+from cpmpy.expressions.variables import NegBoolView
+from ..expressions.core import Comparison, Operator
 
 def remove_redundant(cpm_cons):
     """
@@ -17,3 +18,27 @@ def remove_redundant(cpm_cons):
                 continue
         single_cons.append(cpm_expr)
     return list(set(single_cons))
+
+def horn_clauses(cpm_cons):
+    new_expr_list = []
+
+    for expr in cpm_cons:
+        if isinstance(expr, Operator) and expr.name == 'or':
+            pos_vars = sorted([arg for arg in expr.args if not isinstance(arg, NegBoolView)], key=str)
+            
+            if len(pos_vars) == 1:
+                neg_vars = [~arg for arg in expr.args if isinstance(arg, NegBoolView)]
+                lhs = Operator('and', neg_vars)
+                new_expr = Operator('->', [lhs, pos_vars[0]])
+                new_expr_list.append(new_expr)
+            else:
+                rhs = pos_vars[0]
+                pos_vars.pop(0)
+                neg_vars = [~arg for arg in expr.args if isinstance(arg, NegBoolView)] + [~arg for arg in pos_vars]
+                lhs = Operator('and', neg_vars)
+                new_expr = Operator('->', [lhs, rhs])
+                new_expr_list.append(new_expr)
+        else:
+            new_expr_list.append(expr)
+    return new_expr_list
+    
